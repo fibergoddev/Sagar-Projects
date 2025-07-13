72ae816ebf5ae1457b169c5ad57952396806d795
FiberGod <124904309+fibergoddev@users.noreply.github.com>
2025-07-13 15:20:40 +0530

    Create sw.js

---
+++ PadhaiLikhai/sw.js
@@ -0,0 +1,24 @@
+const cacheName = 'padhailikhai-v1';
+const assetsToCache = [
+  '/',
+  'index.html',
+  'game.html',
+  'style.css',
+  'script.js'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(cacheName).then(cache => {
+      return cache.addAll(assetsToCache);
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
